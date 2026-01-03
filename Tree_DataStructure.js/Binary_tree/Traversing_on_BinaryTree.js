1af5f663cf61fe28e35ad0b6df45fb2074ee95e0
Sohaib Arshid <sohaibali1277@gmail.com>
2026-01-03 21:32:00 +0500

    Daily DSA - 2026-01-03 21:32:00

--- Tree_DataStructure.js/Binary_tree/Traversing_on_BinaryTree.js
+++ Tree_DataStructure.js/Binary_tree/Traversing_on_BinaryTree.js
@@ -6,7 +6,7 @@ class Node {
     }
 }
 
-class searchInBinaryTree {
+class Trversing {
     constructor() {
         this.root = null;
     }
@@ -61,12 +61,67 @@ class searchInBinaryTree {
             return false;
         }
     }
+
+    // level order Treversal
+
+    levelOrderTreversal() {
+        if (!this.root) {
+            return;
+        } else {
+            let queue = [this.root]
+            console.log('level order treversal');
+            while (queue.length > 0) {
+                const currentNode = queue.shift()
+                console.log(currentNode.value);
+                if (currentNode.left) {
+                    queue.push(currentNode.left)
+                }
+                if (currentNode.right) {
+                    queue.push(currentNode.right)
+                }
+            }
+        }
+    }
+
+    // pre order Treversal
+
+    PreOrderTreversal(node = this.root) {
+        if (node) {
+            console.log(node.value);
+            this.PreOrderTreversal(node.left);
+            this.PreOrderTreversal(node.right);
+        }
+    }
+
+    //in order treversal
+    inOrder(node = this.root){
+        if(node){
+            this.inOrder(node.left)
+            console.log(node.value);
+            this.inOrder(node.right)
+        }
+    }
+
+    postorder(node = this.root){
+        if(node){
+            this.postorder(node.left);
+            this.postorder(node.right);
+            console.log(node.value);
+        }
+    }
 }
 
-const Node1 = new searchInBinaryTree()
+const Node1 = new Trversing()
 Node1.add(10)
 Node1.add(20)
 Node1.add(30)
 Node1.add(40)
 Node1.add(50)
 Node1.searchInBTree(50);
+Node1.levelOrderTreversal();
+console.log('pre order treversal');
+Node1.PreOrderTreversal();
+console.log('in order treversal');
+Node1.inOrder();
+console.log('preorder treversal');
+Node1.postorder();
