fafce75f7d9077cbd8e743389ff9871c4fdf43c5
Sohaib Arshid <sohaibali1277@gmail.com>
2026-01-31 18:39:34 +0500

    Daily DSA - 2026-01-31 18:39:34

---
+++ Tree_DataStructure.js/Heap_tree/Heap_Tree.js
@@ -0,0 +1,72 @@
+class MaxHeap {
+    constructor() {
+        this.heap = []
+    }
+
+    getperentindex(i) {
+        return Math.floor((i - 1) / 2)
+    }
+
+    getleftindex(i) {
+        return (2 * i) + 1;
+    }
+
+    getrightindex(i) {
+        return (2 * i) + 2;
+    }
+
+    swap(i1, i2) {
+        const temp = this.heap[i1];
+        this.heap[i1] = this.heap[i2];
+        this.heap[i2] = temp;
+
+        // destructring assigment 
+        // this.heap[i1] , this.heap[i2] = this.heap[i2] , this.heap[i1]  
+    }
+
+    insert(data) {
+        this.heap.push(data);
+        this.bubbleup(this.heap.length - 1);
+    }
+
+    bubbleup(i) {
+        let parentindex = this.getperentindex(i);
+        while (i > 0 && this.heap[i] > this.heap[parentindex]) {
+            this.swap(i, parentindex);
+            i = parentindex;
+            parentindex = this.getperentindex(i)
+        }
+    }
+    print() {
+        console.log(this.heap);
+    }
+
+    remove() {
+        if (this.heap.length === 0) {
+            return;
+        } else if (this.heap.length === 1) {
+            return this.heap.pop()
+        }
+
+        let firstvalue = this.heap[0];
+        this.heap[0] = this.heap.pop()
+        this.heapfydown(0)
+        return firstvalue;
+    }
+
+    heapfydown(i){
+        let largest = i;
+        let leftchild = this.getleftindex(i)
+        let rightchild = this.getrightindex(i)
+    }
+}
+
+const maxheap = new MaxHeap();
+maxheap.insert(12);
+maxheap.insert(13);
+maxheap.insert(14);
+maxheap.insert(15);
+maxheap.insert(16);
+maxheap.insert(17);
+maxheap.insert(18);
+maxheap.print();
